dc90bd51e5d298e4df1fd8ed92a79df98df5c120
IgorGSarmento <igor_gss@hotmail.com>
2018-03-27 16:42:40 -0300

    Adiciona arquivo de configuração do banco, para a aplicação.

---
+++ src/settings/settings.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+  'db' => [
+     'servername' => '172.26.0.2',      //host do container do banco de dados
+     'port' => '3306',					//porta do container do banco de dados
+     'username' => 'teste',				//usuario do banco de dados
+     'password' => 'teste',				//senha do banco de dados
+     'dbname' => 'testedatabase',		//nome do banco de dados
+  ]
+];
